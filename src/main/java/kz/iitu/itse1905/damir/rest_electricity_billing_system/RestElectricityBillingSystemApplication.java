1bfe1eceb6718165c24c64f5506f94f80cd9d43e
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-08 22:33:25 +0600

    create maven project

---
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/RestElectricityBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.itse1905.damir.rest_electricity_billing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestElectricityBillingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestElectricityBillingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/RestElectricityBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.itse1905.damir.rest_electricity_billing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestElectricityBillingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
