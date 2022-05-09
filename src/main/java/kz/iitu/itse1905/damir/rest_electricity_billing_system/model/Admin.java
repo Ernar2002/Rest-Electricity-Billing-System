5c8ff15a2cb848345e2b52eefb4d11e2b5adb855
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-10 03:07:10 +0600

    Admin entity added

---
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/model/Admin.java
@@ -0,0 +1,32 @@
+package kz.iitu.itse1905.damir.rest_electricity_billing_system.model;
+
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.base.BaseEntity;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "roles")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Admin extends BaseEntity {
+
+    private String name;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Email can not be empty")
+    @Email(message = "Please provide a valid email id")
+    private String email;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Password can not be empty")
+    private String pass;
+}
