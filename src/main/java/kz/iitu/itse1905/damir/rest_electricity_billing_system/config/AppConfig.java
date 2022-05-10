f890d1de21efaf0b41168136eb68d17d22c25a1b
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-10 16:29:01 +0600

    Draft version of config changed

--- src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/config/AppConfig.java
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/config/AppConfig.java
@@ -2,15 +2,47 @@ package kz.iitu.itse1905.damir.rest_electricity_billing_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 //@EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class AppConfig {
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/api/auth/login")
+                .usernameParameter("email")
+                .passwordParameter("password");
+//                    .anyRequest().authenticated()
+//                .and()
+//                .apply(new JwtConfigurer(jwtUtil));
+        http.headers().cacheControl();
+    }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
