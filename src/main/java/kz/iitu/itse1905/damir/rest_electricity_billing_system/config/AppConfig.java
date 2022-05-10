17cd887537ae28c092af5476e2cbb3095dca4210
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-10 16:00:17 +0600

    Draft version of config

---
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/config/AppConfig.java
@@ -0,0 +1,19 @@
+package kz.iitu.itse1905.damir.rest_electricity_billing_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
