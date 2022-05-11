abf5e44765a3e4d85b7c31ecf7c5873bc9669ac7
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-12 01:33:29 +0600

    Method getBillsOfUser() added

--- src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/controller/auth/AdminRestController.java
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/controller/auth/AdminRestController.java
@@ -1,9 +1,12 @@
 package kz.iitu.itse1905.damir.rest_electricity_billing_system.controller.auth;
 
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.dto.BillDto;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.dto.ComplaintDto;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.dto.UserDto;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.Bill;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.Complaint;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.User;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.service.BillService;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.service.ComplaintService;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +24,17 @@ public class AdminRestController {
 
     private final UserService userService;
     private final ComplaintService complaintService;
+    private final BillService billService;
 
     @Autowired
     public AdminRestController(
             UserService userService,
-            ComplaintService complaintService
+            ComplaintService complaintService,
+            BillService billService
     ){
         this.userService = userService;
         this.complaintService = complaintService;
+        this.billService = billService;
     }
 
     @GetMapping("/user")
@@ -67,5 +73,27 @@ public class AdminRestController {
         }
 
         return new ResponseEntity<>(complaintDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{userId}/bills")
+    public ResponseEntity<List<BillDto>> getBillsOfUser(@PathVariable("userId") Long id){
+        if(id == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        User user = userService.getById(id);
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        List<Bill> bills = billService.getBillsOfUser(user);
+
+        List<BillDto> billDtos = new ArrayList<>();
+
+        for(Bill bill: bills){
+            billDtos.add(BillDto.fromBill(bill));
+        }
+
+        return new ResponseEntity<>(billDtos, HttpStatus.OK);
     }
 }
