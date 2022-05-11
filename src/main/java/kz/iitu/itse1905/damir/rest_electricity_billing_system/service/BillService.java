448cde244293e9c1b529d9cebf992b3a835363e4
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-12 01:31:06 +0600

    Bill service added

--- src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/service/BillService.java
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/service/BillService.java
@@ -1,2 +1,10 @@
-package kz.iitu.itse1905.damir.rest_electricity_billing_system.service;public class BillService {
+package kz.iitu.itse1905.damir.rest_electricity_billing_system.service;
+
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.Bill;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.User;
+
+import java.util.List;
+
+public interface BillService extends BaseService<Bill>{
+    List<Bill> getBillsOfUser(User user);
 }
