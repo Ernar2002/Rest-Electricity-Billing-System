de9c92bf67f4ec98278ebdc90c36aa50a9ab6a36
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-11 21:18:19 +0600

    Fixed subsequence of test-data.sql

--- src/main/resources/db/test-data.sql
+++ src/main/resources/db/test-data.sql
@@ -6,6 +6,34 @@ INSERT INTO admin (id, name, email, pass, active, created) VALUES
                                               (1, 'Administrator One', 'admin@gmail.com', 'Password123', 'ACTIVE', '2022-10-05')
 
 
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO users (id,
+                       first_name,
+                       last_name,
+                       iin,
+                       email,
+                       phone_number,
+                       password,
+                       address,
+                       active,
+                       created)
+VALUES
+    (1, 'Frederick', 'J Baker', '980126854515', 'baker@gmail.com', '87072564514', 'password', '1488 Franklin Street', 'ACTIVE','2022-05-10'),
+    (2, 'Antonio', 'Dominguez', '890314854515', 'antonio@gmail.com', '87085454755', 'password', '3961 Sycamore Lake Road', 'ACTIVE','2022-05-10'),
+    (3, 'Etta', 'H Abner', '950425854515' , 'etta@gmail.com', '87012569980', 'password', '3255 Ocello Street', 'ACTIVE','2022-05-10'),
+    (4, 'Jeffrey', 'Wegman', '940625854515', 'wegman@gmail.com', '87012458888', 'password', '2962 Pine Tree Lane', 'ACTIVE','2022-05-10'),
+    (5, 'Benjamin', ' Sanderson', '871201854515', 'benjamin@gmail.com', '7012565800', 'password', '4830 Bell Street', 'ACTIVE','2022-05-10'),
+    (6, 'Eric', 'Webb', '840817854515', 'ericw@gmail.com', '87076541000', 'password', '3485 Stewart Street', 'ACTIVE','2022-05-10'),
+    (7, 'Jonathan', 'Lasalle', '990825854515', 'jonathan@gmail.com', '87014585025', 'password', '3850 Olen Thomas Drive', 'ACTIVE','2022-05-10'),
+    (8, 'Liam', 'Moore', '940712854515', 'liamoore@gmail.com', '87012545555', 'password', '744 Ralph Street', 'ACTIVE','2022-05-10'),
+    (9, 'Will', 'Williams', '920404854515', 'williams@gmail.com', '87076969855', 'password', '7855 Allace Avenue', 'ACTIVE','2022-05-10'),
+    (10, 'Christine', 'Moore', '970326854515', 'moore@gmail.com', '87006500010', 'password', '1458 Bleckstreet', 'ACTIVE','2022-05-10'),
+    (11, 'Timothy', ' Diaz', '910824854515', 'timothy@gmail.com', '87071258020', 'password', '4840 Oakdale Avenue', 'ACTIVE','2022-05-10');
+
+
 
 --
 -- Dumping data for table `bill`
@@ -65,31 +93,3 @@ VALUES
 
 INSERT INTO unitsrate (id, twohundred, fivehundred, thousand, active, created) VALUES
     (1, 2, 5, 10, 'ACTIVE', '2022-05-10');
-
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO users (id,
-                   first_name,
-                   last_name,
-                   iin,
-                   email,
-                   phone_number,
-                   password,
-                   address,
-                   active,
-                   created)
-VALUES
-  (1, 'Frederick', 'J Baker', '980126854515', 'baker@gmail.com', '87072564514', 'password', '1488 Franklin Street', 'ACTIVE','2022-05-10'),
-  (2, 'Antonio', 'Dominguez', '890314854515', 'antonio@gmail.com', '87085454755', 'password', '3961 Sycamore Lake Road', 'ACTIVE','2022-05-10'),
-  (3, 'Etta', 'H Abner', '950425854515' , 'etta@gmail.com', '87012569980', 'password', '3255 Ocello Street', 'ACTIVE','2022-05-10'),
-  (4, 'Jeffrey', 'Wegman', '940625854515', 'wegman@gmail.com', '87012458888', 'password', '2962 Pine Tree Lane', 'ACTIVE','2022-05-10'),
-  (5, 'Benjamin', ' Sanderson', '871201854515', 'benjamin@gmail.com', '7012565800', 'password', '4830 Bell Street', 'ACTIVE','2022-05-10'),
-  (6, 'Eric', 'Webb', '840817854515', 'ericw@gmail.com', '87076541000', 'password', '3485 Stewart Street', 'ACTIVE','2022-05-10'),
-  (7, 'Jonathan', 'Lasalle', '990825854515', 'jonathan@gmail.com', '87014585025', 'password', '3850 Olen Thomas Drive', 'ACTIVE','2022-05-10'),
-  (8, 'Liam', 'Moore', '940712854515', 'liamoore@gmail.com', '87012545555', 'password', '744 Ralph Street', 'ACTIVE','2022-05-10'),
-  (9, 'Will', 'Williams', '920404854515', 'williams@gmail.com', '87076969855', 'password', '7855 Allace Avenue', 'ACTIVE','2022-05-10'),
-  (10, 'Christine', 'Moore', '970326854515', 'moore@gmail.com', '87006500010', 'password', '1458 Bleckstreet', 'ACTIVE','2022-05-10'),
-  (11, 'Timothy', ' Diaz', '910824854515', 'timothy@gmail.com', '87071258020', 'password', '4840 Oakdale Avenue', 'ACTIVE','2022-05-10');
