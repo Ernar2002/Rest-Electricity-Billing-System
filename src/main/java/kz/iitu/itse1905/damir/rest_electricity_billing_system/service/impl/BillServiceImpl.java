abe4221babd5394234060cc44418ce40265b07d9
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-14 15:16:40 +0600

    Added method to set bill to user

--- src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/service/impl/BillServiceImpl.java
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/service/impl/BillServiceImpl.java
@@ -1,12 +1,17 @@
 package kz.iitu.itse1905.damir.rest_electricity_billing_system.service.impl;
 
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.Bill;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.EActive;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.EStatus;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.User;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.repository.BillRepository;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.repository.UserRepository;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.service.BillService;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.utils.request.BillRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,8 +25,30 @@ public class BillServiceImpl implements BillService {
     }
 
     @Override
-    public void save(Bill bill) {
+    public void saveBill(BillRequest request, User user) {
+        Bill bill = new Bill();
+        bill.setUnits(request.getUnits());
+        bill.setAmount(request.getAmount());
+        bill.setStartDate(request.getStartDate());
+        bill.setEndDate(request.getEndDate());
+        user.addBill(bill);
+        bill.setUser(user);
+        bill.setStatus(EStatus.PENDING);
+        bill.setActive(EActive.ACTIVE);
+        Date currentDate = new Date();
+        bill.setCreated(currentDate);
 
+        billRepository.save(bill);
+
+    }
+
+
+    @Override
+    public void save(Bill bill) {
+        bill.setStatus(EStatus.PENDING);
+        bill.setActive(EActive.ACTIVE);
+        Date currentDate = new Date();
+        bill.setCreated(currentDate);
     }
 
     @Override
