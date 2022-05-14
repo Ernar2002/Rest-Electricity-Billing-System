d10b134fff1fa568b474b5eb6d9237ca2742e6ab
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-14 15:10:21 +0600

    MessageResponse.java added

---
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/utils/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package kz.iitu.itse1905.damir.rest_electricity_billing_system.utils.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
