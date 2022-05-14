09dc96b31a4d60422c18f7bd0fc936a16d059d76
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-14 15:51:43 +0600

    Method saveBill added

--- src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/service/BillService.java
+++ src/main/java/kz/iitu/itse1905/damir/rest_electricity_billing_system/service/BillService.java
@@ -2,9 +2,11 @@ package kz.iitu.itse1905.damir.rest_electricity_billing_system.service;
 
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.Bill;
 import kz.iitu.itse1905.damir.rest_electricity_billing_system.model.User;
+import kz.iitu.itse1905.damir.rest_electricity_billing_system.utils.request.BillRequest;
 
 import java.util.List;
 
 public interface BillService extends BaseService<Bill>{
     List<Bill> getBillsOfUser(User user);
+    void saveBill(BillRequest request, User user);
 }
